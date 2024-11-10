61983725c6d6559ef10e6f5b222330da20749755
alopalka <alopalka@proton.me>
2024-11-10 15:07:14 +0100

    Enhancements of pipeline

--- lab3-game/test_game.py
+++ lab3-game/test_game.py
@@ -8,12 +8,6 @@ class TestGame(unittest.TestCase):
         self.board = Board(rows=9, cols=9, obstacle_ratio=0.2)
         self.player = Player(self.board.start, self.board)
 
-    def test_game_initialization(self):
-        self.assertIsNotNone(self.board.start)
-        self.assertIsNotNone(self.board.stop)
-        self.assertNotEqual(self.board.start, self.board.stop)
-        self.assertEqual(len(self.board.obstacles), int(25 * 0.2))
-
     def test_player_reaches_goal(self):
         self.player.position = self.board.stop
         self.assertEqual(self.player.get_position(), self.board.stop)
